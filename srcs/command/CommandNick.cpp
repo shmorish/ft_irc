cfe66e978bcb265587807086964b595eb2b26b39
shmorish <110565242+shmorish@users.noreply.github.com>
2024-05-25 01:04:55 +0900

    start nick
    Co-authored-by: Tsubasa Ishikawa <Ishi-eenn@users.noreply.github.com>
    Co-authored-by: Pino <pinohikari@users.noreply.github.com>

--- srcs/command/CommandNick.cpp
+++ srcs/command/CommandNick.cpp
@@ -1,7 +1,6 @@
 #include "Command.hpp"
 
-// void Command::nick(Server &server, User &user)
-// {
-//   (void)
-//   return ;
-// }
+void Command::nick(Server &server, User &user)
+{
+    if (user.get_is_nickname() == true)
+}
