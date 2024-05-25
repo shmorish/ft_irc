de478aa3572715667cb28708c5bab6bd488c9c96
Ishi <syi378039@gmail.com>
2024-05-26 03:17:55 +0900

    nicknameを指定した場合のprivmsgは完成

--- srcs/command/CommandPrivmsg.cpp
+++ srcs/command/CommandPrivmsg.cpp
@@ -1,5 +1,19 @@
 #include "Command.hpp"
 
+// privmsg <nickname> <message>
+static void title(Server &_server, Parser &_parser, User &_user) {
+  if (_server.findUserByNick(_parser.get_args().at(0)) == NULL)
+    throw runtime_error("User not found\n");
+  string message = _user.get_nickname() + " says: ";
+  for (size_t i = 1; i < _parser.get_args().size(); i++) {
+    message += _parser.get_args().at(i);
+    if (i != _parser.get_args().size() - 1)
+      message += " ";
+  }
+  message += "\n";
+  send(_server.findUserByNick(_parser.get_args().at(0))->get_fd(), message.c_str(), message.size(), 0);
+}
+
 void Command::privmsg()
 {
     // check if the user in the channel
@@ -20,12 +34,11 @@ void Command::privmsg()
 
         } else {
             // privmsg <nickname> <message>
-            // nickname
+            title(_server, _parser, _user);
         }
-         
+
     }
     catch (const exception &e) {
         send(_user.get_fd(), e.what(), strlen(e.what()), 0);
     }
 }
-        
