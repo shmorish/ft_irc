eaf558b9b3b9ae376368a07d0794c6720eef28f2
shmorish <110565242+shmorish@users.noreply.github.com>
2024-05-25 15:10:05 +0900

    fix command type

--- srcs/command/Command.cpp
+++ srcs/command/Command.cpp
@@ -1,39 +1,17 @@
 #include "Command.hpp"
 
 Command::Command(Server &server, Parser &parser , User &user_class) : _server(server), _parser(parser), _user(user_class) {
-	switch(_parser.get_command()) {
-		case PASS:
-			pass();
-			break;
-		case NICK:
-			nick();
-			break;
-		case USER:
-			user();
-			break;
-		case JOIN:
-			join();
-			break;
-		case PART:
-			part();
-			break;
-		case PRIVMSG:
-			privmsg();
-			break;
-		case QUIT:
-			quit();
-			break;
-		case PING:
-			ping();
-			break;
-		case PONG:
-			pong();
-			break;
-		default:
-			throw runtime_error("Invalid command");
-	}
+	CommandType cmd = _parser.get_command();
+	if (cmd == PASS) pass();
+	else if (cmd == NICK) nick();
+	else if (cmd == USER) user();
+	else if (cmd == JOIN) join();
+	else if (cmd == PART) part();
+	else if (cmd == PRIVMSG) privmsg();
+	else if (cmd == QUIT) quit();
+	else if (cmd == PING) ping();
+	else if (cmd == PONG) pong();
+	else throw runtime_error("Invalid command");
 }
 
 Command::~Command(){};
-
-
