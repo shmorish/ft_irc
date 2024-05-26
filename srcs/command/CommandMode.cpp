736e20630966564a4ae2e3c1487bec2c628289a8
pino <136807429+pinohikari@users.noreply.github.com>
2024-05-26 18:18:44 +0900

    mode chotto kaeta

--- srcs/command/CommandMode.cpp
+++ srcs/command/CommandMode.cpp
@@ -1,5 +1,19 @@
 #include "Command.hpp"
 
+//mode #channel +o nickname      ＝＞    nickname　にチャンネルオペレータ権限を与えます
+//mode #channel -o nickname       ＝＞    nickname　からチャンネルオペレータ権限を奪います
+//mode #channel +l 100               ＝＞    #channel　の参加数を100に制限します
+//mode #channel -l                       ＝＞    #channel　の参加数制限を解除します
+
+static void is_correct_input(const vector<string> &args){
+    if (args.size() < 2)
+        throw runtime_error("Too few arguments");
+    if (args.size() > 3)
+        throw runtime_error("Too many arguments");
+    if (args.at(0).at(0) != '#')
+        throw runtime_error("Channel name must start with #");
+}
+
 void    Command::mode(){
     // MODE #channel_name <mode>
     try{
@@ -10,8 +24,11 @@ void    Command::mode(){
         if(_parser.get_args().at(0).at(0) == '#' && (_parser.get_args().at(1).at(0) == '+' || _parser.get_args().at(1).at(0) == '-')){
             if(_server.findChannelByName(_parser.get_args().at(0)) == NULL)
                 throw runtime_error("Channel not found");
-        // 自分自身がこのチャンネルに参加しているかどうかを確認
-        // 自分自身がこのチャンネルのオペレーターかどうかを確認
+        Channel *channel = _server.findChannelByName(_parser.get_args().at(0));
+        if(channel->is_client(_user.get_fd()) == false)
+            throw runtime_error("User not found in this channel");
+        if(channel->is_operator(_user.get_fd()) == false)
+            throw runtime_error("You are not operator");
         // <mode>が有効なものかどうかを確認
             if (_parser.get_args().at(1).at(0) == '+' && _parser.get_args().at(1).at(1) == 'l'){
                 _server.findChannelByName(_parser.get_args().at(0))->set_users_limit(atoi(_parser.get_args().at(1).substr(2).c_str()));
