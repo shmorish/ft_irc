b4849590db60387434e75d549093e28ae5761d69
shmorish <110565242+shmorish@users.noreply.github.com>
2024-05-29 00:52:04 +0900

    add ping

--- srcs/command/CommandPing.cpp
+++ srcs/command/CommandPing.cpp
@@ -1,13 +1,16 @@
 #include "Command.hpp"
 
-#define RPL_PING(source, token) ":" + source + " PONG :" + token
-
 void Command::ping(void)
 {
-    // string source = _server.get_server_name();
-    string source = "127.0.0.1";
-    string token = _parser.get_message();
-    string response = RPL_PING(source, token);
-    cout << response << endl;
+    if (_parser.get_args().size() != 1)
+    {
+        string error_message = "PING :Not enough parameters\r\n";
+        send(_user.get_fd(), error_message.c_str(), error_message.size(), 0);
+        return ;
+    }
+    string response;
+    response += "PONG ";
+    response += _parser.get_args().at(0);
+    response += "\r\n";
     send(_user.get_fd(), response.c_str(), response.size(), 0);
 }
