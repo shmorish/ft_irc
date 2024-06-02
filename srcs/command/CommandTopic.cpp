1d1b0765602de02f63d0408eeccb1b7a4d115de4
pino <136807429+pinohikari@users.noreply.github.com>
2024-06-02 12:57:24 +0900

    topic err wo irssi youni shita-yo

--- srcs/command/CommandTopic.cpp
+++ srcs/command/CommandTopic.cpp
@@ -1,32 +1,32 @@
 #include "Command.hpp"
 
-static void is_correct_input(const vector<string> &args){
+static void is_correct_input(const vector<string> &args, User _user){
 	if (args.size() < 2)
-		throw runtime_error("Too few arguments");
+		throw runtime_error(err_461(_user, "TOPIC"));
 	if (args.size() > 3)
 		throw runtime_error("Too many arguments");
 	if (args.at(0).at(0) != '#')
-		throw runtime_error("Channel name must start with #");
+		throw runtime_error(err_476(_user));
 }
 
 void    Command::topic(){
 	// TOPIC #channel_name
 	// TOPIC #channel_name :<topic>
 	try{
-		is_correct_input(_parser.get_args());
+		is_correct_input(_parser.get_args(), _user);
 		if(_server.findChannelByName(_parser.get_args().at(0)) == NULL)
-			throw runtime_error("Channel not found");
+			throw runtime_error(err_476(_user));
 		Channel *channel = _server.findChannelByName(_parser.get_args().at(0));
 		if(channel->is_client(_user.get_fd()) == false)
-			throw runtime_error("User not found in this channel");
+			throw runtime_error(err_442(_user, channel->get_channel_name()));
 		if(_parser.get_args().size() == 2){
-			string topic = _server.findChannelByName(_parser.get_args().at(0))->get_topic();
+			string topic = channel->get_topic();
 			send(_user.get_fd(), topic.c_str(), topic.size(), 0);
 		}
 		if(_parser.get_args().size() == 3){
-			if(channel->is_operator(_user.get_fd()) == false)
-				throw runtime_error("You are not operator");
-			_server.findChannelByName(_parser.get_args().at(0))->set_topic(_parser.get_args().at(1).substr(1));
+			if((channel->get_mode() & Channel::TopicOpOnly) && channel->is_operator(_user.get_fd()) == false)
+				throw runtime_error(err_482(_user, channel->get_channel_name()));
+			channel->set_topic(_parser.get_args().at(1).substr(1));
 		}
 	}catch(const exception &e){
 		send(_user.get_fd(), e.what(), strlen(e.what()), 0);
